be1a97d0429664111435191db1979d58fc811ed2
Ossian Eriksson <ossian.eriksson@elev.ga.ntig.se>
2023-04-11 14:41:26 +0200

    Create index.php

---
+++ index.php
@@ -0,0 +1,42 @@
+<?php
+//Create and connect to database
+$db = new SQLite3("chirpbase.sq3");
+//Create account table if it doesn't already exist
+$db -> exec(
+    "CREATE TABLE IF NOT EXISTS Accounts (
+    AccountID INTEGER PRIMARY KEY AUTOINCREMENT,
+    Name TEXT,
+    Email TEXT,
+    Password TEXT,
+    FollowerCount INTEGER,
+    FollowedCount INTEGER
+  )");
+  //Account ID is used to keep track of what the user does. and make it easy to find the right user.
+
+//Create Posts table
+$db->exec('CREATE TABLE Posts (
+    PostID INTEGER PRIMARY KEY AUTOINCREMENT,
+    AccountID INTEGER,
+    Content TEXT,
+    Likes INTEGER,
+    FOREIGN KEY (AccountID) REFERENCES Accounts(AccountID)
+)');
+//PostID is used for moderation so we can easily send the post to a list
+//Create Comments Table
+$db->exec('CREATE TABLE Comments (
+    CommentID INTEGER PRIMARY KEY AUTOINCREMENT,
+    PostID INTEGER, 
+    AccountID INTEGER,
+    Content TEXT,
+    Likes INTEGER,
+    FOREIGN KEY (AccountID) REFERENCES Accounts(AccountID)
+    FOREIGN KEY (PostID) REFERENCES Posts(PostID)
+)'); 
+
+ /* 
+  $sql = "INSERT INTO Accounts (Name, Email, Password, FollowerCount, FollowedCount) VALUES ('$name', '$email', '$password', $follower_count, $followed_count)";
+   */
+// close the database connection
+$db->close();
+
+?>
